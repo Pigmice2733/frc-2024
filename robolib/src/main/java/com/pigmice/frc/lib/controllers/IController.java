3b2cb60c1ea2336a63ba3726526f3c00e82f4b2a
LucaHaverty <lucahaverty@gmail.com>
2023-12-29 22:06:37 -0800

    Initial commit pt 2

--- robolib/src/main/java/com/pigmice/frc/lib/controllers/IController.java
+++
@@ -1,8 +0,0 @@
-package com.pigmice.frc.lib.controllers;
-
-import com.pigmice.frc.lib.motion.setpoint.ISetpoint;
-
-public interface IController {
-    public void initialize(double input, double currentOutput);
-    public double calculateOutput(double input, ISetpoint setpoint);
-}
--- robolib/src/main/java/com/pigmice/frc/lib/inputs/IBooleanSource.java
+++
@@ -1,6 +0,0 @@
-package com.pigmice.frc.lib.inputs;
-
-public interface IBooleanSource {
-    boolean get();
-    default void update() {}
-}
